c8c49d6bda659a78e8572492869d5c2751dd0bb9
Yi Chen Hock <yichenhock@gmail.com>
2022-07-17 20:50:27 +0100

    add slash commands

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ require('dotenv').config({
   path: '../.env'
 });
 
-//const discord = require('discord.js');
-
 const { Client, Intents } = require("discord.js");
 const client = new Client({ 
   intents: [
@@ -12,13 +10,48 @@ const client = new Client({
   ] 
 });
 
+const clientId = process.env.CLIENT_ID;
+const guildId = process.env.GUILD_ID;
+const token = process.env.BOT_TOKEN;
+
+// deploy commands here
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
+
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+		await rest.put(
+			// Routes.applicationGuildCommands(clientId, guildId),
+			Routes.applicationCommands(clientId),
+			{ body: commands },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
+
+// set up bot
+
 client.login(process.env.BOT_TOKEN).then((token) => {
   client.user.setActivity('being talented', {
     type: 'PLAYING', 
   });
 });
-
-
 
 client.once('ready', ()=> {
   console.log(new Date() + ' Chen Bot has started!');
@@ -31,3 +64,16 @@ client.on("messageCreate", async message => {
   }
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
